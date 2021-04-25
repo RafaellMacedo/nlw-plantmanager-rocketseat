d344a777d26a12024c2d1615048e616dc3b627c0
Rafael Macedo <rafaelldemacedo@gmail.com>
2021-04-25 19:40:07 -0300

    Iniciando o projeto do curso do evento Next Level Week 5

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, TouchableOpacityProps } from 'react-native';
+import colors from '../styles/colors';
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string;
+}
+
+export function Button({ title, ... rest } : ButtonProps ){
+    return (
+        <TouchableOpacity 
+        style={styles.button} 
+        activeOpacity={0.7}
+        { ... rest}
+    >
+        <Text style={styles.buttonText}>
+            { title }
+        </Text>
+    </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 25,
+        height: 56,
+        width: 56,
+        paddingHorizontal: 10
+    },
+    buttonText: {
+        color: colors.white,
+        fontSize: 24
+    }
+})
